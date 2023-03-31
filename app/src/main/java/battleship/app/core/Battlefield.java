bd47f662b496ad032e1f03496987f7d429fa1b1a
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2023-03-31 22:29:44 +1100

    Upgrade to Java version 20

--- app/src/main/java/battleship/app/core/Battlefield.java
+++ app/src/main/java/battleship/app/core/Battlefield.java
@@ -21,8 +21,8 @@ public class Battlefield extends GridPane {
         setVgap(1);
 
         // Cells
-        for (int row = 0; row < 10; row++) {
-            for (int col = 0; col < 10; col++) {
+        for (int row = 0; row < SIZE.row(); row++) {
+            for (int col = 0; col < SIZE.column(); col++) {
                 AreaRef ref = new AreaRef(col, row);
                 Cell cell = new Cell(ref);
                 add(cell, col, row);
